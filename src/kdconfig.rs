ae578fc9b165ab07d29815d687bad556060db3fd
Scott Rice <meris608@gmail.com>
2022-11-18 16:11:13 -0600

    Support adding comments to config files

--- src/kdconfig.rs
+++ src/kdconfig.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
+use json_comments::StripComments;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -20,9 +21,7 @@ pub struct KdConfig {
 impl KdConfig {
   pub fn parse(path: PathBuf, file: File) -> Result<KdConfig> {
     let reader = BufReader::new(file);
-
-    let parsed_command_mapping: HashMap<String, SubcommandConfig> = serde_json::from_reader(reader)
-        .context("Failed to parse config file as JSON")?;
+    let parsed_command_mapping = KdConfig::parse_commands(reader)?;
 
     let kdconfig = KdConfig {
       root_path: path,
@@ -30,4 +29,59 @@ impl KdConfig {
     };
     Ok(kdconfig)
   }
+
+  fn parse_commands<T: Read>(reader: T) -> Result<HashMap<String, SubcommandConfig>> {
+    // Strip comments out of the JSON file, so that people can add comments
+    // to their config files without triggering an error.
+    let stripped = StripComments::new(reader);
+    // Use serde to parse our mapping
+    let result: HashMap<String, SubcommandConfig> = serde_json::from_reader(stripped)
+      .context("while parsing config file.")?;
+
+    // TODO: There should be a way to directly return the result of
+    // `from_reader` but I think the error types get messed up.
+    Ok(result)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parsing_json() {
+      let data = r#"
+        {
+            "hello": {
+              "cmd": "echo Hello World!",
+              "help": "Prints a friendly message"
+            }
+        }"#;
+      let result = KdConfig::parse_commands(data.as_bytes());
+      assert!(result.is_ok());
+
+      let mapping = result.unwrap();
+      assert!(mapping.get("hello").is_some());
+      assert!(mapping.get("world").is_none());
+    }
+
+    #[test]
+    fn test_parsing_json_with_comments() {
+      let data = r#"
+        {
+            // This is a helpful comment about a helpful command
+            "hello": {
+              /* Notice that we use the same phrase everyone else does?
+                 That's on purpose. */
+              "cmd": "echo Hello World!",
+              "help": "Prints a friendly message"
+            }
+        }"#;
+      let result = KdConfig::parse_commands(data.as_bytes());
+      assert!(result.is_ok());
+
+      let mapping = result.unwrap();
+      assert!(mapping.get("hello").is_some());
+      assert!(mapping.get("world").is_none());
+    }
 }
