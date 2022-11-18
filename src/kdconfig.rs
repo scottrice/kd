63badb2a1a093ab70fb6b59a27255a127db3afca
Scott Rice <meris608@gmail.com>
2022-11-18 15:39:10 -0600

    Move config structs and parsing into a new file

---
+++ src/kdconfig.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct SubcommandConfig {
+    pub cmd: String,
+    pub help: String,
+}
+
+pub struct KdConfig {
+  pub root_path: PathBuf,
+  pub commands_by_name: HashMap<String, SubcommandConfig>,
+}
+
+impl KdConfig {
+  pub fn parse(path: PathBuf, file: File) -> Result<KdConfig> {
+    let reader = BufReader::new(file);
+
+    let parsed_command_mapping: HashMap<String, SubcommandConfig> = serde_json::from_reader(reader)
+        .context("Failed to parse config file as JSON")?;
+
+    let kdconfig = KdConfig {
+      root_path: path,
+      commands_by_name: parsed_command_mapping,
+    };
+    Ok(kdconfig)
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::collections::HashMap;
+mod kdconfig;
+
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
 
+use anyhow::{anyhow, Context, Result};
 use colored::Colorize;
 use itertools::Itertools;
-use serde::Deserialize;
 use shell_words;
 
-use anyhow::{anyhow, Context, Result};
+use kdconfig::{KdConfig, SubcommandConfig};
 
 // `kd` lets you create easy-to-remember and ergonomic CLI commands at a
 // per-project level. These can be checked into source control and shared by every
@@ -31,19 +31,13 @@ USAGE:
   $ kd <subcommand> <subcommand arguments>
 ";
 
-#[derive(Debug, Deserialize)]
-struct SubcommandConfig {
-    cmd: String,
-    help: String,
-}
-
-fn print_help(config: Option<HashMap<String, SubcommandConfig>>) -> Result<()> {
+fn print_help(maybe_config: Option<KdConfig>) -> Result<()> {
     print!("{HELP_PRELUDE}");
 
-    if config.is_none() {
+    if maybe_config.is_none() {
         return Ok(());
     }
-    let config_values = config
+    let config = maybe_config
         .expect("Config should be Some since we just checked");
 
     // TODO: Skip this section if there are no subcommands in the file
@@ -52,10 +46,11 @@ fn print_help(config: Option<HashMap<String, SubcommandConfig>>) -> Result<()> {
     println!("");
     println!("AVAILABLE SUBCOMMANDS:");
 
-    for command in config_values.keys().sorted() {
+    for command in config.commands_by_name.keys().sorted() {
         let bolded_command = command.bold().underline();
 
-        let command_definition = config_values
+        let command_definition = config
+            .commands_by_name
             .get(command)
             .expect("We should have been given a valid key");
         let command_help = &command_definition.help;
@@ -70,7 +65,7 @@ fn print_help(config: Option<HashMap<String, SubcommandConfig>>) -> Result<()> {
     Ok(())
 }
 
-fn find_config() -> (Option<PathBuf>, Option<File>) {
+fn find_config() -> Option<(PathBuf, File)> {
     let mut path = std::env::current_dir()
         .expect("Cannot access current directory");
     let filename = Path::new(".kdconfig");
@@ -82,7 +77,7 @@ fn find_config() -> (Option<PathBuf>, Option<File>) {
             Ok(file) => {
                 // Remove filename from the path, leaving the directory.
                 path.pop();
-                return (Some(path), Some(file))
+                return Some((path, file));
             }
             // File not found will be given when there is no .kdconfig in the
             // current directory. In that scenario, just move up a directory
@@ -91,25 +86,16 @@ fn find_config() -> (Option<PathBuf>, Option<File>) {
                 // If one of these calls fails, it means we have reached the
                 // root of the filesystem and didn't find anything.
                 if !(path.pop() && path.pop()) {
-                    return (None, None)
+                    return None;
                 }
             }
             // TODO: We found a file but can't access it (likely for
             // permissions reasons). We should give the user a better error
             // message here, but right now we return the same thing as if we
             // couldn't find a kdconfig at all.
-            Err(_) => return (None, None),
+            Err(_) => return None,
         }
     }
-}
-
-fn parse_config(file: File) -> Result<HashMap<String, SubcommandConfig>> {
-    let reader = BufReader::new(file);
-
-    let config: HashMap<String, SubcommandConfig> = serde_json::from_reader(reader)
-        .context("Failed to parse config file as JSON")?;
-
-    Ok(config)
 }
 
 fn execute_cmd(config: &SubcommandConfig, config_directory: &Path) -> Result<i32> {
@@ -135,12 +121,12 @@ fn execute_cmd(config: &SubcommandConfig, config_directory: &Path) -> Result<i32
     status.code().ok_or_else(|| anyhow!("Process terminated by signal"))
 }
 
-fn run_subcommand(config_optional: Option<HashMap<String, SubcommandConfig>>, subcommand: &str, config_directory: &Path) -> Result<i32> {
-    if let Some(config) = config_optional {
+fn run_subcommand(maybe_config: Option<KdConfig>, subcommand: &str) -> Result<i32> {
+    if let Some(config) = maybe_config {
         // We do have a `.kdconfig`.
-        if let Some(subcommand_config) = config.get(subcommand) {
+        if let Some(subcommand_config) = config.commands_by_name.get(subcommand) {
             // And we have an entry for the subcommand the user entered.
-            let exitcode = execute_cmd(subcommand_config, config_directory)
+            let exitcode = execute_cmd(subcommand_config, &config.root_path)
                 .context("while running subcommand")?;
             Ok(exitcode)
         } else {
@@ -162,11 +148,9 @@ fn main() -> Result<()> {
 
     let subcommand = &args[1];
 
-    // TODO: Clean this up and put it in an struct with the actual config values
-    let (config_directory, config_file) = find_config();
-
-    let config = config_file
-        .map(|path| parse_config(path))
+    let nearest_config_file = find_config();
+    let config = nearest_config_file
+        .map(|(path, file)| KdConfig::parse(path, file))
         .transpose()
         .context("Failed to parse the config file")?;
 
@@ -177,7 +161,7 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    let exitcode = run_subcommand(config, subcommand, &config_directory.expect("Expecting a directory"))
+    let exitcode = run_subcommand(config, subcommand)
         .context("while trying to run the subcommand")?;
     exit(exitcode);
 }
