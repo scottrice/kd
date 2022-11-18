69c08dda41ea0d8553dc3a3b1ce4ff7ad9f836a8
Scott Rice <meris608@gmail.com>
2022-11-18 15:44:56 -0600

    Remove unused import

--- src/kdconfig.rs
+++ src/kdconfig.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use serde::Deserialize;
